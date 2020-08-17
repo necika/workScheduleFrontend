4ed86b23e5149ba246b3054a928f1e15b742db59
Nemanja <nemanja.salipurevic@gmail.com>
2020-08-17 15:20:47 +0200

    Timesheet page added, AddUsers page added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { TimesheetComponent } from './timesheet/timesheet.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
+    TimesheetComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/users/addUsers/addUsers.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-addUsers',
+  templateUrl: './addUsers.component.html',
+  styleUrls: ['./addUsers.component.css']
+})
+export class AddUsersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
